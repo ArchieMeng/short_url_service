e8a81311a1beaf33165d6bf86ea247114a6a91b4
archiemeng <myt1996037@gmail.com>
2017-09-16 19:21:46 +0800

    Revert "initialize project"
    This reverts commit c2c73c392a598ca2fb421db6f3d083d01b41f646.

--- run.py
+++
@@ -1,9 +0,0 @@
-import short_url_service
-
-if __name__ == "__main__":
-    short_url_service.app.run(
-        host='0.0.0.0',
-        port=short_url_service.port,
-        threaded=True,
-        debug=False
-    )
